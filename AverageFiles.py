ffd0139a5d311e903f8bb3aab637efe59749f0ed
Wolf <wolf@zv.cx>
2015-05-05 14:47:12 -0400

    more plumbing, less porcelain
    teach all_non_merge_commits to limit the number of commits returned;
    rename some internal variables, adjust comments

--- AverageFiles.py
+++ AverageFiles.py
@@ -9,7 +9,7 @@ from contextlib import contextmanager
 @contextmanager
 def use_directory( path=None ):
     """A Context Manager to change to the given directory for the duration of the 'with' context.
-       When path is None, don't change directories.
+    When path is None, don't change directories.
     """
     olddir = os.getcwd()
     try:
@@ -20,30 +20,34 @@ def use_directory( path=None ):
         os.chdir(olddir)
 
 
-def all_non_merge_commits( git_dir=None ):
-    """a generator yielding 40-hex-digit strings that are the hashes of commits from the object database"""
-
+def all_non_merge_commits( git_dir=None, limit=None ):
+    """a generator yielding 40-hex-digit commit hash strings"""
+    args = ['git', 'rev-list', '--all', '--no-merges']
+    if type(limit)==int:
+        args += ['-{}'.format(limit)]
     with use_directory(git_dir):
-        out = check_output(['git', 'log', '--all', '--no-merges', '--format=%H'])
-    for hash in out.splitlines():
-        yield hash.decode('utf-8')
+        out = check_output(args)
+    for h in out.decode('utf-8').splitlines():
+        yield h
 
 
 def count_files_committed( git_dir=None ):
-    """Return a collections.Counter dictionary where the keys are file-counts and the values are the number of commits that touched that exact number of files."""
-
+    """Return a collections.Counter dictionary where the keys are file-counts and the values
+    are the number of commits that touched exactly that many files.
+    """
     counter = Counter()
     with use_directory(git_dir):
         for h in all_non_merge_commits():
-            out = check_output(['git', 'log', '-1', '--format=', '--name-only', h])
-            count = len(out.splitlines())
-            counter[count] += 1
+            out = check_output(['git', 'log', '--no-walk', '--format=', '--name-only', h])
+            num_files_touched = len(out.splitlines())
+            counter[num_files_touched] += 1
     return counter
 
 
 def average_files_per_commit( counter ):
-    """Given a dictionary as returned by count_files_committed(), return the average number of files per commit."""
-
+    """Given a dictionary as returned by count_files_committed(),
+    return the average number of files touched per commit.
+    """
     total_files = 0
     total_commits = 0
 
@@ -52,6 +56,7 @@ def average_files_per_commit( counter ):
         total_commits += num_commits
 
     return total_files / total_commits
+
 
 if __name__ == '__main__':
     path = sys.argv[1] if len(sys.argv)>1 else None
